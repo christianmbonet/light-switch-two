228b3e5c8cf38e518ddf45138ba5e99ca2001b51
christianmbonet <christianm.bonet@gmail.com>
2021-05-01 14:49:16 -0400

    colors and alternating switches

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,30 @@
-import React from "react"
-
-
 import React, { useState } from "react";
 import './../components/index.css'
 
 export default function Home() {
    
   const [on, setOn] = useState(false);
+  const [initialColor, setInitialColor] = useState(false);
 
   const lightSwitchCenter = () => setOn(on => !on);
-  const lightSwitchPink = () => setOn(on => !on);
+  const lightSwitchPink = () => setInitialColor(initialColor => !initialColor);
 
   return <div className='page'>
   <div className='container'>
     <div className='topRow'>
-        <button onClick={lightSwitchPink} className={on ? 'topLeftPink' : 'topLeftLightBlue'}></button>
-        <div className={on ? 'topMiddleBlue' : 'topMiddlePink'}></div>
-        <button onClick={lightSwitchPink} className={on ? 'topRightPink' : 'topRightLightBlue'}></button>
+        <button onClick={lightSwitchPink} className={initialColor ? 'topLeftPink' : 'topLeftLightBlue'}></button>
+        <div className={initialColor ? 'topMiddleBlue' : 'topMiddlePink'}></div>
+        <button onClick={lightSwitchPink} className={initialColor ? 'topRightPink' : 'topRightLightBlue'}></button>
     </div>
     <div className='middleRow'>
-        <div className={on ? 'middleLeftBlue': 'middleLeftPink'}></div>
-        <button onClick={lightSwitchCenter} className='middleMiddle'></button>
-        <div className={on ? 'middleRightBlue' : 'middleRightPink'}></div>
+        <div className={initialColor ? 'middleLeftBlue': 'middleLeftPink'}></div>
+        <button onClick={lightSwitchCenter} className={on ? 'middleMiddle' : 'middleMiddleBlack'}></button>
+        <div className={initialColor ? 'middleRightBlue' : 'middleRightPink'}></div>
     </div>
     <div className='bottomRow'>
-        <button onClick={lightSwitchPink} className={on ? 'bottomLeftPink' : 'bottomLeftLightBlue'}></button>
-        <div className={on ? 'bottomMiddleBlue' : 'bottomMiddlePink'}></div>
-        <div onClick={lightSwitchPink} className={on? 'bottomRightPink' : 'bottomRightLightBlue'}></div>
+        <button onClick={lightSwitchPink} className={initialColor ? 'bottomLeftPink' : 'bottomLeftLightBlue'}></button>
+        <div className={initialColor ? 'bottomMiddleBlue' : 'bottomMiddlePink'}></div>
+        <div onClick={lightSwitchPink} className={initialColor ? 'bottomRightPink' : 'bottomRightLightBlue'}></div>
     </div>
   </div>
   </div>
